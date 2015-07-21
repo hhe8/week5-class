4c83fcefcafaf5f3e2d2072b9bbd4865667cd5ca
Jeff Cohen <cohen.jeff@gmail.com>
2015-07-21 17:13:35 -0500

    Ready for class (I hope)

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,41 @@
+class MoviesController < ApplicationController
+
+  def update
+    movie = Movie.find_by(:id => params["id"])
+    movie.title = params[:title]
+    movie.year = params[:year]
+    movie.save
+    redirect_to root_url
+  end
+
+  def edit
+    @movie = Movie.find_by(:id => params["id"])
+  end
+
+  def new
+    logger.debug Movie.count.to_s
+  end
+
+  def create
+    movie = Movie.new
+    movie.title = params["title"]
+    movie.year = params["year"]
+    movie.save
+    redirect_to root_url # "/"
+  end
+
+  def index
+    @movies = Movie.all
+  end
+
+  def destroy
+    @movie = Movie.find_by(:id => params["id"])
+    @movie.delete
+    redirect_to "/movies"
+  end
+
+  def show
+    @movie = Movie.find_by(:id => params["id"])
+  end
+
+end

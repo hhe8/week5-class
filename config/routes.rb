f4fda6588de7bea18d00063807cc25e1cbf16233
Jeff Cohen <cohen.jeff@gmail.com>
2015-07-21 20:09:27 -0500

    The Golden 7 Routes!

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,9 @@ Rails.application.routes.draw do
 
   root 'movies#index'
 
-  # This is the Golden 7 for Movies
-  get '/movies' => 'movies#index'
-  get '/movies/new' => 'movies#new'
-  post '/movies' => 'movies#create'
-  get '/movies/:id' => 'movies#show'
-  get '/movies/:id/edit' => 'movies#edit'
-  patch '/movies/:id' => 'movies#update'
-  delete '/movies/:id' => 'movies#destroy'
-
-  # This is the Golden 7 for Directors
-  get '/directors' => 'directors#index'
-  get '/directors/new' => 'directors#new'
-  post '/directors' => 'directors#create'
-  get '/directors/:id' => 'directors#show'
-  get '/directors/:id/edit' => 'directors#edit'
-  patch '/directors/:id' => 'directors#update'
-  delete '/directors/:id' => 'directors#destroy'
+  resources :movies
+  resources :directors
+  resources :actors
 
 
 end
